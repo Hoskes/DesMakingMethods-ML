bcca904a16d4ecabb37878284b0c6f1af17579fb
Hoskis <vip.izyavich@mail.ru>
2025-02-20 22:54:26 +0300

    Update L3.py

--- L3/L3.py
+++ L3/L3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from keras import Input
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import Sequential
@@ -27,11 +28,13 @@ y_train_scaled = scaler_y.fit_transform(y_train)
 y_test_scaled = scaler_y.transform(y_test)
 
 # 3. Создание модели
-model = Sequential()
-model.add(Dense(32, activation='relu', input_shape=(1,)))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(1))
-model.compile(optimizer='sgd', loss='mse')
+model = Sequential([
+    Input(shape=(1,)),
+    Dense(32, activation='relu'),
+    Dense(32, activation='relu'),
+    Dense(1)
+])
+model.compile(optimizer='adam', loss='mse')
 
 # 4. Обучение модели
 history = model.fit(
